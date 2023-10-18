bbdabff7342deb6bc14ddc533d293670b4bde320
Kevin Johr <kevin.johr@posteo.de>
2023-10-18 23:07:01 +0200

    Projektdateien hinzufügen.

---
+++ Data/Database/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Bot_Balu_Ass_DB.Data.Database
+{
+    internal class ApplicationDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=BotBaluAssDB;Trusted_Connection=True;MultipleActiveResultSets=true");
+        }
+    }
+}
