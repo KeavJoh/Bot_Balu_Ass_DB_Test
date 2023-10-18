a9356fa12a70eaa85b3e1e2e756d7af2fb395aa9
Kevin Johr <kevin.johr@posteo.de>
2023-10-18 23:16:02 +0200

    add create database on startup

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using Bot_Balu_Ass_DB.Data.Database;
+
+class Program
+{
+    static void Main()
+    {
+        using (var context = new ApplicationDbContext())
+        {
+            context.Database.EnsureCreated();
+        }
+
+    }
+}
