1766c9b16d020c409ef4c4ac043e3e4ceab86755
Kevin Johr <kevin.johr@posteo.de>
2023-10-19 21:07:13 +0200

    add _ChannelSettings

---
+++ BotSettingsModels/_ChannelSettings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bot_Balu_Ass_DB.BotSettingsModels
+{
+    internal class _ChannelSettings
+    {
+        public long ParentsCommandChannel {  get; set; }
+        public long ParentsInformationChannel { get; set; }
+        public long ExecutiveCommandChannel { get; set; }
+        public long ExecutiveInformationChannel { get; set; }
+        public long EmployeesCommandChannel { get; set; }
+        public long EmployeesInformationChannel { get; set; }
+
+    }
+}
