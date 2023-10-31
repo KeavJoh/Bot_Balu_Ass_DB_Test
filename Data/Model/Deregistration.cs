77f747f74dbe53fe37f8d3639f7ae1bc01a23b30
Kevin Johr <kevin.johr@posteo.de>
2023-10-31 22:24:08 +0100

    implement Deregistration Table and Model

---
+++ Data/Model/Deregistration.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bot_Balu_Ass_DB.Data.Model
+{
+    internal class Deregistration
+    {
+        public int Id { get; set; }
+        public string ChildName { get; set; }
+        public int ChildId { get; set; }
+        public DateTime? DeregistrationAt { get; set; }
+        public DateTime? DeregistrationFrom { get; set; }
+        public DateTime? DeregistrationTo { get; set;}
+        public bool DeregistrationForOneDay { get; set; }
+        public bool DeregistrationPerformedFromParents { get; set; }
+    }
+}
---
+++ Migrations/20231031212226_implementDeregistrationTable.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bot_Balu_Ass_DB.Migrations
+{
+    /// <inheritdoc />
+    public partial class implementDeregistrationTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "deregistrations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ChildName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ChildId = table.Column<int>(type: "int", nullable: false),
+                    DeregistrationAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DeregistrationFrom = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DeregistrationTo = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DeregistrationForOneDay = table.Column<bool>(type: "bit", nullable: false),
+                    DeregistrationPerformedFromParents = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_deregistrations", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "deregistrations");
+        }
+    }
+}
