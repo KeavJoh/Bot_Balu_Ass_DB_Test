d420afaeac8b14416b884de0b799d2e6b343bc76
Kevin Johr <kevin.johr@posteo.de>
2023-11-03 21:58:54 +0100

    improvements

--- Controller/HelpFunctionController.cs
+++ Controller/HelpFunctionController.cs
@@ -17,23 +17,34 @@ namespace Bot_Balu_Ass_DB.Controller
             switch (caseInt)
             {
                 case 1:
-                    var chanelId1 = await client.GetChannelAsync(GlobalSettings.BotConfig.ChannelSettings.ExecutiveCommandChannel);
-                    await DeleteLastMessageInSelectedChannel(chanelId1);
+                    var channelId1 = await client.GetChannelAsync(GlobalSettings.BotConfig.ChannelSettings.ExecutiveCommandChannel);
+                    await DeleteLastMessageInSelectedChannel(channelId1);
                     break;
                 case 2:
-                    var chanelId2 = await client.GetChannelAsync(GlobalSettings.BotConfig.ChannelSettings.ParentsCommandChannel);
-                    await DeleteLastMessageInSelectedChannel(chanelId2);
+                    var channelId2 = await client.GetChannelAsync(GlobalSettings.BotConfig.ChannelSettings.ParentsCommandChannel);
+                    await DeleteLastMessageInSelectedChannel(channelId2);
                     break;
             }
         }
 
-        private static async Task DeleteLastMessageInSelectedChannel(DiscordChannel chanelId)
+        //Delete all messages from given channel
+        public static async Task DeleteAllMessagesFromChannel(DiscordChannel discordChannel)
         {
-            var messages = await chanelId.GetMessagesAsync(1);
+            var allMessagesInChannel = await discordChannel.GetMessagesAsync();
+            if (allMessagesInChannel.Count > 0)
+            {
+                await discordChannel.DeleteMessagesAsync(allMessagesInChannel);
+            }
+        }
+
+        private static async Task DeleteLastMessageInSelectedChannel(DiscordChannel channelId)
+        {
+            var messages = await channelId.GetMessagesAsync(1);
             var latestMessage = messages.FirstOrDefault();
             await latestMessage.DeleteAsync();
         }
 
+        //Parse strinf DateTime to DateTime
         public static DateTime ParseStringToDateTime(string stringDateTime)
         {
             DateTime finalDateTime;
@@ -47,16 +58,6 @@ namespace Bot_Balu_Ass_DB.Controller
                 finalDateTime = DateTime.MinValue;
             }
             return finalDateTime;
-        }
-
-        //Delete all messages from given channel
-        public static async Task DeleteAllMessagesFromChannel(DiscordChannel discordChannel)
-        {
-            var allMessagesInChannel = await discordChannel.GetMessagesAsync();
-            if (allMessagesInChannel.Count > 0)
-            {
-                await discordChannel.DeleteMessagesAsync(allMessagesInChannel);
-            }
         }
     }
 }
